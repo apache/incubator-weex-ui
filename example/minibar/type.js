ced262266f9c77f67223487654695fbe70a8f737
Tw93 <tw93@qq.com>
2017-11-07 12:12:23 +0800

    change icon

--- example/minibar/type.js
+++ example/minibar/type.js
@@ -2,5 +2,5 @@
  * Created by Tw93 on 2016/10/29.
  */
 export const CART_ICON =
-  'https://gw.alicdn.com/tfs/TB1Y9vlpwMPMeJjy1XcXXXpppXa-72-72.png';
-export const RETURN_ICON = 'https://gw.alicdn.com/tfs/TB1PtN3pwMPMeJjy1XdXXasrXXa-72-72.png';
+  'https://gw.alicdn.com/tfs/TB1HrvtbBTH8KJjy0FiXXcRsXXa-32-32.png';
+export const RETURN_ICON = 'https://gw.alicdn.com/tfs/TB1cAYsbv2H8KJjy0FcXXaDlFXa-30-53.png';
