e79b2038006277dc372d539ef546eea086203181
Tw93 <tw93@qq.com>
2017-12-14 16:30:18 +0800

    [+] update  minPixelValue: 1.01

--- webpack.config.js
+++ webpack.config.js
@@ -116,7 +116,10 @@ webCfg.module.rules[1].use.push({
       require('autoprefixer')({
         browsers: ['> 0.1%', 'ios >= 8', 'not ie < 12']
       }),
-      require('postcss-plugin-px2rem')({ rootValue: 75 })
+      require('postcss-plugin-px2rem')({
+        rootValue: 75,
+        minPixelValue: 1.01
+      })
     ],
     compilerModules: [
       {
