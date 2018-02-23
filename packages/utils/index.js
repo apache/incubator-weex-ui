a8f959ccf1559eacfc7fb7c2b637e2e6cbfeae9c
cixiaoxiaole <gaojiajie@cxxiaole.com>
2018-02-23 12:49:30 +0800

    抽离页面转场动画代码，新增日历和城市选择转场push和model模式动画

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -156,6 +156,14 @@ const Utils = {
       const { env } = weex.config;
       const navHeight = Utils.env.isWeb() ? 0 : (Utils.env.isIPhoneX() ? 176 : 132);
       return env.deviceHeight / env.deviceWidth * 750 - navHeight;
+    },
+    /**
+     * 获取weex屏幕真实的设置高度
+     * @returns {Number}
+     */
+    getScreenHeight () {
+      const { env } = weex.config;
+      return env.deviceHeight / env.deviceWidth * 750;
     }
   },
 
@@ -227,6 +235,44 @@ const Utils = {
       newStr += '...';
     }
     return newStr;
+  },
+  animation: {
+    /**
+     * 返回定义页面转场动画起初的位置
+     * @param ref
+     * @param transform 运动类型
+     * @param status
+     * @param callback 回调函数
+     */
+    pageTransitionAnimation (ref, transform, status, callback) {
+      const animation = weex.requireModule('animation');
+      animation.transition(ref, {
+        styles: {
+          transform: transform
+        },
+        duration: status ? 250 : 300, // ms
+        timingFunction: status ? 'ease-in' : 'ease-out',
+        delay: 0 // ms
+      }, function () {
+        callback && callback();
+      });
+    }
+  },
+  uiStyle: {
+    /**
+     * 返回定义页面转场动画起初的位置
+     * @param animationType 页面转场动画的类型 push，model
+     * @param size 分割数组的长度
+     * @returns {}
+     */
+    pageTransitionAnimationStyle (animationType) {
+      if (animationType === 'push') {
+        return { left: '750px', top: '0px', height: (weex.config.env.deviceHeight / weex.config.env.deviceWidth * 750) + 'px' }
+      } else if (animationType === 'model') {
+        return { top: (weex.config.env.deviceHeight / weex.config.env.deviceWidth * 750) + 'px', left: '0px', height: (weex.config.env.deviceHeight / weex.config.env.deviceWidth * 750) + 'px' }
+      }
+      return {}
+    }
   }
 };
 
