db29f264b786a24f9e2a39410b9868d8b67e610e
zwwill <zwwill7@163.com>
2018-02-23 20:46:48 +0800

    [+] add popover

--- example/popover/type.js
+++ example/popover/type.js
@@ -2,6 +2,6 @@
  * Created by Tw93 on 2016/10/29.
  */
 export const RETURN_ICON = 'https://gw.alicdn.com/tfs/TB1cAYsbv2H8KJjy0FcXXaDlFXa-30-53.png';
-export const SCAN_ICON = 'https://gw.alipayobjects.com/zos/rmsportal/tOtXhkIWzwotgGSeptou.svg';
-export const QRCODE_ICON = 'https://gw.alipayobjects.com/zos/rmsportal/PKAgAqZWJVNwKsAJSmXd.svg';
-export const HELP_ICON = 'https://gw.alipayobjects.com/zos/rmsportal/uQIYTFeRrjPELImDRrPt.svg';
+export const SCAN_ICON = 'http://cdn.zwwill.com/weexui/icon/scan.png';
+export const QRCODE_ICON = 'http://cdn.zwwill.com/weexui/icon/qr_code.png';
+export const QUESTION_ICON = 'http://cdn.zwwill.com/weexui/icon/question.png';
