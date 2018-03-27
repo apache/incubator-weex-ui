d9193127b768dac4cb6d8f0385326bbd07bcac25
Tw93 <tw93@qq.com>
2018-03-27 20:29:06 +0800

    [ ! ] review code

--- packages/wxc-refresher/index.vue
+++ packages/wxc-refresher/index.vue
@@ -3,26 +3,32 @@
 <template>
   <refresh ref="wxc-refresher"
            class="wxc-refresher"
-           @refresh="onrefresh"
-           @pullingdown="onPullingdown"
+           @refresh="onRefresh"
+           @pullingdown="onPullingDown"
            :display="refreshing ? 'show' : 'hide'">
     <div v-if="newStyleFlag" class="cycle-container" ref="cycle">
       <div class="u-cover c1" ref="cover1">
-        <div class="u-cover-cycle cover1"></div></div>
+        <div class="u-cover-cycle cover1"></div>
+      </div>
       <div class="u-cover c2" ref="cover2">
         <div class="u-cover-cycle" ref="cover-cycle"></div>
       </div>
     </div>
-    <image v-if="newStyleFlag" class="arrow-down" ref="arrow" :src="ICON_ARROW_DOWN" resize="contain"></image>
-    <loading-indicator v-if="!newStyleFlag" class="indicator"></loading-indicator>
-    <text class="u-txt" :style="{width:`${textWidth}px`}" lines="1">{{refresherText}}</text>
+    <image v-if="newStyleFlag"
+           class="arrow-down"
+           ref="arrow"
+           :src="ICON_ARROW_DOWN"
+           resize="contain"></image>
+    <loading-indicator v-else class="indicator"></loading-indicator>
+    <text class="u-txt" :style="{width:`${textWidth}px`}">{{refresherText}}</text>
   </refresh>
 </template>
 <script>
-  const animation = weex.requireModule('animation')
-  const modal = weex.requireModule('modal')
+  const animation = weex.requireModule('animation');
 
-  import bindingx from 'weex-bindingx';
+  // 减少打包体积
+  import bindingx from 'weex-bindingx/lib/index.weex';
+  import BindEnv  from '../utils/bind-env';
   import Utils from '../utils';
 
   const ICON_ARROW_DOWN = 'https://img.alicdn.com/tfs/TB1A8faeTtYBeNjy1XdXXXXyVXa-48-48.png';
@@ -34,57 +40,52 @@
         type: Number,
         default: 0
       },
-      mainText:{
-        tyle: String,
+      mainText: {
+        type: String,
         default: '下拉即可刷新...'
       },
-      pullingText:{
-        tyle: String,
+      pullingText: {
+        type: String,
         default: '释放即可刷新...'
       },
-      refreshingText:{
-        tyle: String,
+      refreshingText: {
+        type: String,
         default: '加载中...'
       },
-      textWidth:{
+      textWidth: {
         type: Number,
         default: 200
       }
     },
     data () {
       return {
-        isAndroid:Utils.env.isAndroid(),
-        isIOS:Utils.env.isIOS(),
         ICON_ARROW_DOWN,
         refreshing: false,
-        couldUnlash: false
+        couldUnLash: false
       }
     },
-    computed:{
-      isSupportBinding: function(){
-        return bindingx && bindingx.isSupportBinding
+    computed: {
+      newStyleFlag () {
+        return this.scrollerRef && BindEnv.supportsEBForIos();
       },
-      newStyleFlag: function(){
-        return this.scrollerRef && !this.isAndroid && this.isSupportBinding
+      refresherText () {
+        return this.refreshing ? this.refreshingText : this.pText;
       },
-      refresherText: function () {
-        return this.refreshing?this.refreshingText:this.pText;
-      },
-      pText: function () {
-        return this.couldUnlash?this.pullingText:this.mainText;
+      pText () {
+        return this.couldUnLash ? this.pullingText : this.mainText;
       }
     },
-    created(){
+    created () {
       this.newStyleFlag && this.animationBinding();
     },
-    beforeDestroy(){
-      this.bindingsDestory();
+    beforeDestroy () {
+      this.bindingsDestroy();
     },
     methods: {
-      onrefresh (event) {
+      onRefresh (event) {
         this.refreshing = true;
         this.newStyleFlag && this.cycleGoRound();
-        if(this.maxTime<=0) return;
+        if (this.maxTime <= 0) return;
         clearTimeout(this.timeoutSto);
         this.timeoutSto = setTimeout(() => {
           this.$emit('wxcTimeout');
@@ -96,124 +97,108 @@
        */
       wxcCancel () {
         this.refreshing = false;
-        this.roundingDestory();
+        this.roundingDestroy();
       },
       /**
        * 下拉事件
        */
-      onPullingdown (event) {
-        let pd = event.pullingDistance * (!!this.isIOS?-1:1);
-        pd>(!!this.isAndroid?200:140)? (this.couldUnlash = true) : (this.couldUnlash = false);
-        if(this.refreshing && pd<20){
+      onPullingDown (event) {
+        let pd = event.pullingDistance * (Utils.env.isIOS() ? -1 : 1);
+        pd > (Utils.env.isAndroid() ? 200 : 140) ? (this.couldUnLash = true) : (this.couldUnLash = false);
+        if (this.refreshing && pd < 20) {
           clearTimeout(this.timeoutSto);
           this.refreshing = false;
-          this.roundingDestory();
+          this.roundingDestroy();
         }
       },
       /**
        * 注册 bindingx
        */
-      animationBinding(){
-        if(this.isAndroid) return;
+      animationBinding () {
         setTimeout(() => {
-          let scroller = this.$parent.$refs[this.scrollerRef].ref,
-              cover2 = this.$refs['cover2'].ref,
-              coverCycle = this.$refs['cover-cycle'].ref;
-          if(!bindingx || !bindingx.isSupportBinding){
-            return;
-          }
-          let bindingResult = bindingx.bind({
-            eventType:'scroll',
+          // 降级版本取不到，需要注意
+          const scroller = this.$parent.$refs[this.scrollerRef].ref;
+          const cover2 = this.$refs['cover2'].ref;
+          const coverCycle = this.$refs['cover-cycle'].ref;
+
+          const bindingResult = bindingx.bind({
+            eventType: 'scroll',
             anchor: scroller,
             props: [
               {
                 element: cover2,
-                property:'transform.rotateZ',
-                expression:'y>-140?(y%75/150*-360):156'
+                property: 'transform.rotateZ',
+                expression: 'y>-140?(y%75/150*-360):156'
               },
               {
                 element: coverCycle,
-                property:'opacity',
-                expression:'y<-75?1:0'
+                property: 'opacity',
+                expression: 'y<-75 ?1:0'
               }
             ]
-          },function(e){
-          });
+          }, (e) => {});
           this.bindingToken = bindingResult.token;
-        },300);
+        }, 300);
       },
       /**
        * 旋转动作
        */
-      cycleGoRound(){
-        if(this.isAndroid) return;
+      cycleGoRound () {
+        if (Utils.env.isAndroid()) return;
         let cycle = this.$refs['cycle'].ref;
         this.arrowShow(false);
-        if(!cycle || !bindingx || !bindingx.isSupportBinding){
+        if (!cycle) {
           return;
         }
-        let roundingResult = bindingx.bind({
-          eventType:'timing',
+        const roundingResult = bindingx.bind({
+          eventType: 'timing',
           props: [
             {
               element: cycle,
-              property:'transform.rotateZ',
-              expression:'t*0.72'
+              property: 'transform.rotateZ',
+              expression: 't*0.72'
             }
           ]
-        },function(e){
+        }, (e) => {
         });
         this.roundingToken = roundingResult.token;
       },
       /**
        * 箭头显隐控制
        */
-      arrowShow(_show=true){
-        let arrow = this.$refs['arrow'];
-        !!arrow && animation.transition(arrow, {
+      arrowShow (_show = true) {
+        const arrow = this.$refs['arrow'];
+        arrow && animation.transition(arrow, {
           styles: {
-            opacity: _show?1:0,
-            transform: _show?"scale(1)":"scale(0.5)"
+            opacity: _show ? 1 : 0,
+            transform: _show ? "scale(1)" : "scale(0.5)"
           },
-          duration:300,
+          duration: 300,
           delay: 0
-        }, () => {
-        });
-      },
-      /**
-       * 安卓动画hack
-       */
-      androidStyleShow(_show=true){
-        let coverCycle = this.$refs['cover-cycle'];
-        !!coverCycle && animation.transition(coverCycle, {
-          styles: {
-            opacity: _show?1:0
-          },
-          duration:200
         }, () => {
         });
       },
       /**
        * 完整 bindingx 销毁
        */
-      bindingsDestory(){
-        if(this.bindingToken != 0) {
+      bindingsDestroy () {
+        if (this.bindingToken !== 0) {
           bindingx && bindingx.unbind({
-            eventType:'scroll',
-            token:this.bindingToken
+            eventType: 'scroll',
+            token: this.bindingToken
           });
           this.bindingToken = 0;
         }
-        this.roundingDestory();
+        this.roundingDestroy();
       },
       /**
        * 旋转 bindingx 销毁
        */
-      roundingDestory(){
-        if(this.roundingToken != 0) {
+      roundingDestroy () {
+        if (this.roundingToken !== 0) {
           bindingx && bindingx.unbind({
-            eventType:'timing',
-            token:this.roundingToken
+            eventType: 'timing',
+            token: this.roundingToken
           });
           this.roundingToken = 0;
         }
@@ -224,7 +209,7 @@
 </script>
 <style scoped>
 
-  .wxc-refresher{
+  .wxc-refresher {
     height: 140px;
     width: 750px;
     flex-direction: row;
@@ -232,29 +217,34 @@
     justify-content: center;
     padding-top: 50px;
   }
-  .cycle-container{
+
+  .cycle-container {
     position: relative;
     width: 60px;
     height: 60px;
   }
-  .u-cover{
+
+  .u-cover {
     position: absolute;
     width: 30px;
     height: 60px;
     top: 0;
-    background-color: #fff;
+    background-color: #ffffff;
     overflow: hidden;
     right: 0;
   }
-  .c1{
+
+  .c1 {
     z-index: 1;
   }
-  .c2{
+
+  .c2 {
     z-index: 2;
     transform-origin: left center;
     transform: rotateZ(0deg);
   }
-  .u-cover-cycle{
+
+  .u-cover-cycle {
     position: absolute;
     width: 60px;
     height: 60px;
@@ -262,31 +252,35 @@
     top: 0;
     box-sizing: border-box;
     border-width: 2px;
-    border-color: #666;
+    border-color: #666666;
     border-style: solid;
     border-radius: 100%;
     opacity: 0;
   }
-  .cover1{
+
+  .cover1 {
     opacity: 1;
   }
-  .indicator{
+
+  .indicator {
     margin-right: 20px;
     height: 60px;
     width: 60px;
-    color: #666;
+    color: #666666;
   }
-  .arrow-down{
+
+  .arrow-down {
     position: relative;
     top: 15px;
     left: -45px;
     width: 30px;
     height: 30px;
   }
-  .u-txt{
+
+  .u-txt {
     font-size: 24px;
     line-height: 40px;
-    color: #999;
+    color: #999999;
     margin-top: 10px;
     margin-left: 10px;
     height: 40px;
