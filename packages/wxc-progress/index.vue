397d70d46aae5d4bf9fdbe84f79feeff3376831c
wjun94 <kasepa1994@gmail.com>
2019-04-26 16:56:54 +0800

    [!] wxc-progress添加props bgdColor可以修改背景色

--- packages/wxc-progress/index.vue
+++ packages/wxc-progress/index.vue
@@ -43,15 +43,20 @@
       value: {
         type: Number,
         default: 0
+      },
+      bgdColor: {
+        type: String,
+        default: '#f2f3f4'
       }
     },
     computed: {
       runWayStyle () {
-        const { barWidth, barHeight, barRadius } = this;
+        const { barWidth, barHeight, barRadius, bgdColor } = this;
         return {
           width: barWidth + 'px',
           height: barHeight + 'px',
-          borderRadius: barRadius + 'px'
+          borderRadius: barRadius + 'px',
+          backgroundColor: bgdColor
         }
       },
       progressStyle () {
