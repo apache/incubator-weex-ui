f14029407001fbca361e6df65c1ffaaf03723d1e
wjun94 <kasepa1994@gmail.com>
2019-04-29 11:33:15 +0800

    添加wxc-nested-list嵌套滑动组件

---
+++ packages/wxc-nested-list/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <scroller ref="wxcNestedListDom"
+      @loadmore="onLoadmore" 
+      loadmoreoffset='200'
+      @touchstart="(e) => $emit('onTouchstart', e)"
+      @touchmove="(e) => $emit('onTouchmove', e)"
+      @touchend="(e) => $emit('onTouchend', e)"
+      @click="$emit('onListClick')"
+      :style="styles"
+      :scroll-direction="isLevel && 'horizontal'" 
+      :class="['wxc-nested-list', isLevel && 'level-list']" 
+      :pagingEnabled ="isPagingEnabled"
+      shouldStopPropagationInitResult="false" 
+      shouldStopPropagationInterval="1000000" 
+      @stoppropagation="Env.platform === 'iOS' ? false : true" 
+      show-scrollbar=false 
+      :scrollable="true" >
+    <slot></slot>
+  </scroller>
+</template>
+
+<script>
+export default {
+  props: {
+    styles: {
+      type: Object,
+      default: {}
+    },
+    isLevel: {
+      type: Boolean,
+      default: false
+    },
+    isResetLoadMore: {
+      type: Boolean,
+      default: false
+    },
+    isPagingEnabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      Env: WXEnvironment
+    };
+  },
+  methods: {
+    onLoadmore() {
+      this.$emit("onLoadmore");
+      if (this.isResetLoadMore) {
+        setTimeout(() => {
+          this.$refs.wxcNestedListDom.resetLoadmore();
+        }, 500);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.wxc-nested-list {
+  height: 200px;
+}
+
+.level-list {
+  flex-direction: row;
+}
+</style>
