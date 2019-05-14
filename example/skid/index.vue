ad210628e5a2833f1de67fb120119e38c892ccb4
wjun94 <kasepa1994@gmail.com>
2019-05-14 12:24:46 +0800

    [+] 添加skid案例

---
+++ example/skid/index.vue
@@ -0,0 +1,86 @@
+<template>
+<div>
+    <WxcSkid @onNodeClick='onTest' :data='data'/>
+</div>
+</template>
+
+<script>
+import WxcSkid from '../../index';
+const modal = weex.requireModule("modal");
+
+export default {
+  data() {
+    return {
+      data: [
+        {
+          title: "标题1",
+          autoClose: true,
+          right: [
+            {
+              text: "置顶",
+              onPress: function() {
+                modal.toast({
+                  message: "置顶",
+                  duration: 0.3
+                });
+              }
+            },
+            {
+              text: "删除",
+              onPress: () => {
+                modal.toast({
+                  message: "删除",
+                  duration: 0.3
+                });
+              },
+              style: { backgroundColor: "#F4333C", color: "white" }
+            }
+          ]
+        },
+        {
+          title: "标题23",
+          right: [
+            {
+              text: "删除",
+              onPress: () => {
+                modal.toast({
+                  message: "删除",
+                  duration: 0.3
+                });
+              },
+              style: { backgroundColor: "#F4333C", color: "white" }
+            }
+          ]
+        },
+        {
+          title: "标题3",
+          right: [
+            {
+              text: "删除",
+              onPress: () => {
+                modal.toast({
+                  message: "删除",
+                  duration: 0.3
+                });
+              },
+              style: { backgroundColor: "#F4333C", color: "white" }
+            }
+          ]
+        }
+      ]
+    };
+  },
+  components: {
+    WxcSkid
+  },
+  methods: {
+    onTest(node, i) {
+      modal.toast({
+        message: node.title,
+        duration: 0.3
+      });
+    }
+  }
+};
+</script>
+
